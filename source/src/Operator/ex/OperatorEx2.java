066842688f6057e940e4bf50a59b7dfff9dadfe3
nahowo <nahowo@naver.com>
2025-03-13 11:44:24 +0900

    섹션 4

---
+++ source/src/Operator/ex/OperatorEx2.java
@@ -0,0 +1,14 @@
+package Operator.ex;
+
+public class OperatorEx2 {
+    public static void main(String[] args) {
+        double num1 = 1.5;
+        double num2 = 2.5;
+        double num3 = 3.5;
+
+        double sum = num1 + num2 + num3;
+        double average = sum / 3;
+        System.out.println("sum = " + sum);
+        System.out.println("average = " + average);
+    }
+}
